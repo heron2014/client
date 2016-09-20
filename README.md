ec29e4465f09935d77ea01066ce1c966d9e31579
Anita <a.nita@hotmail.co.uk>
2016-09-20 12:38:19 +0100

    set up the project refs #1

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# client
-Client (React and Redux)
+# client (React and Redux)
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: require('path').join(__dirname, 'public'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    root: __dirname,
+    modulesDirectories: [
+      'node_modules',
+      './src/components'
+    ],
+    alias: {
+      src: 'src'
+    },
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './public'
+  },
+  devtool: 'cheap-module-eval-source-map'
+};
